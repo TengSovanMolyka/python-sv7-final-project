ae52c58dcbd4254786bca3bdc1f0e9ee74b9f8be
Teng Sovanmolyka <tengsovanmolyka09@gmail.com>
2026-06-17 17:19:56 +0700

    Integrate Vercel KV for user data storage
    Add Vercel KV integration for user data management.

--- app.py
+++ app.py
@@ -12,6 +12,11 @@ from product import (products as pro,
                      update_stock)
 import json, random, os, uuid
 import requests
+
+from vercel_kv import kv
+kv.set("users", users)
+users = kv.get("users")
+
 from werkzeug.utils import secure_filename
 app = Flask(__name__)
 app.secret_key = "your_secret_key"  # needed for flash messages
